ae15e7d492476e228f8b776cc5d1a015c0c1785b
coskunyerli <coskun.yerli@gmail.com>
2023-10-02 00:26:59 +0300

    Edit login page and otp page

--- src/app/otp-entry/otp-entry.component.ts
+++ src/app/otp-entry/otp-entry.component.ts
@@ -11,7 +11,12 @@ import { AppSettings } from '../constants';
 })
 export class OtpEntryComponent {
 
-  otpValue: string = '';
+  otpValue1: string = '';
+  otpValue2: string = '';
+  otpValue3: string = '';
+  otpValue4: string = '';
+  otpValue5: string = '';
+  otpValue6: string = '';
   contact: string | null = ''
   constructor(private http: HttpClient, private authService: AuthService, public route: ActivatedRoute, private router: Router) {
     route.data.subscribe(data => console.log(data));
@@ -28,18 +33,26 @@ export class OtpEntryComponent {
         //this.router.navigate(['no_page'])
         // show no page
       } else {
-        this.http.post<any>(`${AppSettings.BASE_URL}/login/verify`, { contact: this.contact, otp: this.otpValue }).subscribe({
-          next: data => {
-            this.authService.loginUser(data.token);
-            let redirectUrl = localStorage.getItem('redirectUrl');
-            if (!!redirectUrl) {
-              this.router.navigate([redirectUrl]);
+        this.http.post<any>(`${AppSettings.BASE_URL}/login/verify`,
+          {
+            contact: this.contact,
+            otp: this.otpValue1 + this.otpValue2 + this.otpValue3 + this.otpValue4 + this.otpValue5 + this.otpValue6
+          }).subscribe({
+
+            next: data => {
+              this.authService.loginUser(data.token);
+              let redirectUrl = localStorage.getItem('redirectUrl');
+              if (!!redirectUrl) {
+                this.router.navigate([redirectUrl]);
+              }
+              else {
+                this.router.navigate(['/my_account'])
+              }
+            },
+            error: error => {
+              console.error('There was an error!', error);
             }
-          },
-          error: error => {
-            console.error('There was an error!', error);
-          }
-        });
+          });
       }
 
     }
