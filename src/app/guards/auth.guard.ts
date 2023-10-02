7acdd45b5d9032f2270068b9a191bcc25c34a254
coskunyerli <coskun.yerli@gmail.com>
2023-10-02 09:02:08 +0300

    Added back-end authentication check

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,29 +1,30 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, createUrlTreeFromSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { AppSettings } from '../constants';
+import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   createUrlTreeFromSnapshot
-  let isAuth = inject(AuthService).isAuthenticated();
-
-  if (!isAuth) {
-    localStorage.setItem('redirectUrl', state.url);
-    return createUrlTreeFromSnapshot(route, ['/', 'login']);
-  } else {
-    // let http = inject(HttpClient);
-    // http.get(`${AppSettings.BASE_URL}/login`).pipe(tap(data => { return true }),
-    //   catchError((error) => { return false }));
-    return true;
-  }
+  let authService = inject(AuthService);
+  let isAuth = authService.isAuthenticated();
+  return isAuth.pipe(
+    map((res: boolean) => {
+      if (!res) {
+        localStorage.setItem('redirectUrl', state.url);
+        return createUrlTreeFromSnapshot(route, ['/', 'login'])
+      } else {
+        return true;
+      }
+    }));
 
 };
 
 
 export const hasAuthGuard: CanActivateFn = (route, state) => {
   createUrlTreeFromSnapshot
-  let isAuth = inject(AuthService).isAuthenticated();
+  let isAuth = inject(AuthService).isLocalAuthenticated();
   if (isAuth) {
     return createUrlTreeFromSnapshot(route, ['/', 'my_account']);
   } else {
